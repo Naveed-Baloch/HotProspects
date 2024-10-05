f673febc822ec6edae0ef56e5937a48908d46015
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-10-05 16:17:08 +0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Naveed on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
