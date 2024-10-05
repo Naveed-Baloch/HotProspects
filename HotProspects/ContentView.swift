4b642b33732aa1ac3ea9b4683d450f2a8d03775e
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-10-05 16:42:46 +0500

    make the list selectable

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let users = ["Naveed", "ali", "Shahzad", "Momin"]
+    @State private var selectedUser: String? = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            List(users, id: \.self, selection: $selectedUser) { user in
+                Text(user)
+            }
+            if let selectedUser = selectedUser {
+                Text(selectedUser)
+            }
+            
         }
         .padding()
     }
