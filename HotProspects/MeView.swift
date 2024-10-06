4baab214a022500e7cc2705c30fdaea96d047f48
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-10-06 22:03:21 +0500

    Setup basic View

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -9,7 +9,25 @@ import Foundation
 import SwiftUI
 
 struct MeView: View {
+    @AppStorage("name") private var name = "Anonymous"
+    @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+
     var body: some View {
-        Text("Me View")
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+            }
+            .navigationTitle("Your code")
+        }
     }
+}
+
+#Preview {
+    MeView()
 }
