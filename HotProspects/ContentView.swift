82f26ee79d8d64efbd32896cb7899cf8f5f010ad
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-10-06 19:26:23 +0500

    show Local Notification using User Notification

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     @State private var selectedTab = "One"
@@ -53,6 +54,33 @@ struct ContentView: View {
                                 print("delete item")
                             }
                         }
+                }
+                
+                Button("Request Notification Permission") {
+                    UNUserNotificationCenter.current()
+                        .requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                            if success {
+                                print("All set!")
+                            } else if let error {
+                                print(error.localizedDescription)
+                            }
+                        }
+                }
+                
+                Button("Show Notification") {
+                    let content = UNMutableNotificationContent()
+                    content.title = "Feed the cat"
+                    content.subtitle = "It looks hungry"
+                    content.sound = UNNotificationSound.default
+
+                    // show this notification five seconds from now
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                    // choose a random identifier
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                    // add our notification request
+                    UNUserNotificationCenter.current().add(request)
                 }
             }
             .tabItem {
