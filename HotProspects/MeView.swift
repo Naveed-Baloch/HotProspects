787e18e89d06b382c7bad0fc0ccfe19e06869f72
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-10-06 22:19:10 +0500

    Generate the QR Code using CI Image filters

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
-
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -22,9 +25,27 @@ struct MeView: View {
                 TextField("Email address", text: $emailAddress)
                     .textContentType(.emailAddress)
                     .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
             }
             .navigationTitle("Your code")
         }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
